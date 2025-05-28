3ed2955339f0f88b8fa7e1808fdc592a2946a9b5
TheBunnyMan123 <bunny@tkbunny.net>
2025-05-27 22:55:41 -0500

    Handle more errors

--- src/commands/roll_dice.rs
+++ src/commands/roll_dice.rs
@@ -1,5 +1,5 @@
 use poise::serenity_prelude as serenity;
-use tyche::{dice::{roller::FastRand as FastRandRoller}, Expr, expr::{Evaled, CalcError}};
+use tyche::{dice::{roller::FastRand as FastRandRoller}, Expr, expr::{CalcError}};
 use crate::types::{Error, Context};
 
 #[poise::command(
@@ -9,18 +9,32 @@ use crate::types::{Error, Context};
 )]
 pub async fn roll_dice(ctx: Context<'_>, equation: String) -> Result<(), Error> {
     let mut roller = FastRandRoller::default();
-    let expr = equation.parse::<Expr>().map_err(|e: tyche::parse::Error| -> Error {format!("Invalid dice notation: {}", e).into()})?;
-    let rolled: Result<Evaled, _> = expr.eval(&mut roller).map_err(|e| -> Error {format!("Error while rolling dice: {}", e).into()});
-    let result = rolled.unwrap().calc().map_err(|e: CalcError| -> Error {format!("Error while calculating result: {}", e).into()});
+    let expr = &equation.parse::<Expr>().map_err(|e: tyche::parse::Error| -> Error {format!("Invalid dice notation: {}\n\n**Tips:**\n- Values greater than 255 are not supported - this is a limitation with the typesrary being used", e).into()})?;
 
-    let embed = serenity::builder::CreateEmbed::default()
-        .title("Rolled your dice!")
-        .field("Equation", &equation, true)
-        .field("Result", result.unwrap().to_string(), true)
-        .field("", "Library: [tyche-rs](https://github.com/Gawdl3y/tyche-rs) ([License](https://github.com/Gawdl3y/tyche-rs/blob/main/LICENSE.md))", false)
-        .color(0xFFAAEE);
+    
+    let rolled = match expr.eval(&mut roller) {
+        Ok(val) => val,
+        Err(e) => {
+        let e_msg = e.to_string();
 
-    ctx.send(poise::CreateReply::default().embed(embed)).await?;
+        let message = if let Some(first_colon_space_idx) = &e_msg.find(": ") {
+            e_msg[first_colon_space_idx + 2..].to_string()
+        } else {
+            e_msg
+        };
+
+        return Err(format!("Error rolling the dice: {}", message).into())
+    }};
+
+            let result = rolled.calc().map_err(|e: CalcError| -> Error {format!("Error while calculating result: {}", e).into()});
+            let embed = serenity::builder::CreateEmbed::default()
+                .title("Rolled your dice!")
+                .field("Equation", &equation, true)
+                .field("Result", result.unwrap().to_string(), true)
+                .field("", "Library: [tyche-rs](https://github.com/Gawdl3y/tyche-rs) ([License](https://github.com/Gawdl3y/tyche-rs/blob/main/LICENSE.md))", false)
+                .color(0xFFAAEE);
+
+            ctx.send(poise::CreateReply::default().embed(embed)).await?;
     Ok(())
 }
 
